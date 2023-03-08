befd83c35998b38c1adf459d349ae1febafeac75
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-03-08 13:27:37 -0500

    Type of arrays

--- Lab2/Mux.sv
+++ Lab2/Mux.sv
@@ -4,15 +4,15 @@ module Mux #(
 )
 (
     //Ports definition
-    input logic unsigned [N-1:0] Src2; //Signals for Mux1, Mux2
+    input logic signed [N-1:0] Src2; //Signals for Mux1, Mux2
     input logic unsigned [1:0] RDst3; //Signal for Mux1
-    input logic unsigned [N-1:0] RD2; //Signal for Mux2
-    input logic unsigned [N-1:0] R; //Signal for Mux3
-    input logic unsigned [N-1:0] ReadData; //Signal for Mux3
+    input logic signed [N-1:0] RD2; //Signal for Mux2
+    input logic signed [N-1:0] R; //Signal for Mux3
+    input logic signed [N-1:0] ReadData; //Signal for Mux3
     
     output logic unsigned [1:0] RA2; //Output signal for Mux1
-    output logic unsigned [N-1:0] B; //Output signal for Mux2
-    output logic unsigned [N-1:0] Result; //Output signal for Mux3
+    output logic signed [N-1:0] B; //Output signal for Mux2
+    output logic signed [N-1:0] Result; //Output signal for Mux3
     
     input logic unsigned WE; //Mux1 control
     input logic unsigned ALUSrc2; //Mux2 control
