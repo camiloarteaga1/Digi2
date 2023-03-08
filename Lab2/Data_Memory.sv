effe8a267cf016d7a036a98fc2bb4436d0869897
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-03-08 13:52:19 -0500

    Memory creation
    Missing some functions for the peripherals

--- Lab2/Data_Memory.sv
+++ Lab2/Data_Memory.sv
@@ -1,13 +1,23 @@
 module Data_Memory #(
-    parameter N = 8; //Number of bits for the arrays
+    parameter N = 8, M = 8; //Number of bits for the arrays and Number of rows for the memory
 ) (
-    input logic unsigned [N-1:0] Addr; //Memory address
-    input logic unsigned [N-1:0] WriteData; //Memory address
+    input logic signed [N-1:0] Addr; //Memory address
+    input logic signed [N-1:0] WriteData; //Memory address
     input logic WE; //Write Enable
     input logic clk; //Clock
-    input logic ReadP2; //Switch
-    input logic unsigned [N-1:0] ReadP3; //Read data
-    output logic 
+    input logic ReadP2; //Button
+    input logic signed [N-1:0] ReadP3; //Switchs
+    output logic signed [N-1:0] ReadData; //Read Data
+    output logic signed [N-1] WriteP1; //Seven segments display
 );
     
+    logic [N-1:0] RAM [2**M-1:0];
+
+    always_ff @(posedge clk) begin
+        if (WE)
+            RAM[Addr] <= WriteData
+        end
+
+    assign ReadData = RAM[Addr];
+
 endmodule
