be5d0eb13678771e50408e96345b822371ef0fd7
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-03-08 21:15:52 -0500

    Memory module (Complete?)

--- Lab2/Data_Memory.sv
+++ Lab2/Data_Memory.sv
@@ -1,5 +1,5 @@
 module Data_Memory #(
-    parameter N = 8, M = 8; //Number of bits for the arrays and Number of rows for the memory
+    parameter N = 8, M = 8; //Number of bits for the arrays, and number of rows for the memory
 ) (
     input logic signed [N-1:0] Addr; //Memory address
     input logic signed [N-1:0] WriteData; //Memory address
@@ -11,13 +11,17 @@ module Data_Memory #(
     output logic signed [N-1] WriteP1; //Seven segments display
 );
     
-    logic [N-1:0] RAM [2**M-1:0];
+    reg [N-1:0] RAM [2**M-1:0];
 
     always_ff @(posedge clk) begin
-        if (WE)
-            RAM[Addr] <= WriteData
-        end
 
-    assign ReadData = RAM[Addr];
+        if (WE) begin
+            RAM[Addr] <= WriteData
+        end        
+    end
+
+    assign WriteP1 = (Addr == 253) && (WE) ? RAM[Addr] : (_);
+    assign ReadData = (Addr == 254) && (!WE) ? {RAM[Addr][N-1:1], ReadP2} : RAM[Addr];
+    assign ReadData = (Addr == 255) && (!WE) ? ReadP3 : RAM[Addr];
 
 endmodule
