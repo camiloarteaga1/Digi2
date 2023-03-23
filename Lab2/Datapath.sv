c77e05e9b935a01709dfd6d3b883ea80137774dc
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-03-23 13:51:16 -0500

    datapath tb

--- Lab2/Datapath.sv
+++ Lab2/Datapath.sv
@@ -38,3 +38,41 @@ module Datapath #(
     Data_Memory Memory (.Addr(Result), .WriteData(WriteData), .WE(WE), .clk(CLK), .ReadP2(ReadP2), .ReadP3(ReadP3), .ReadData(ReadData), .WriteP1(WriteP1));
 
 endmodule
+
+module Datapath_tb();
+    
+    //Port definitions
+    localparam N = 8;
+    logic [1:0] RSrc1, //Array to select register
+    logic [1:0] RDst3, //Array to select register
+    logic Reset, //Reset button
+    logic CLK, //Clock
+    logic [N-1:0] Src2, //Array with data
+    logic WE, //Write enable signal
+    logic ALUSrc2, //Mux2 control
+    logic ALUorM, //Mux3 control
+    logic [2:0] ALUCntr, //ALU operation control
+    logic ReadP2, //Button
+    logic [N-1:0] ReadP3, //Switchs
+    logic [3:0] ALUFlags, //Flags
+    logic [N-1:0] WriteP1 //Seven segments
+
+    Datapath datap(.RSrc1(RSrc1), .RDst3(RDst3), .Reset(~Reset), .CLK(CLK), .Src2(Src2), .WE(WE), .ALUSrc2(ALUSrc2), .ALUorM(ALUorM), .ALUCntr(ALUCntr), .ReadP2(ReadP2), .ReadP3(ReadP3), .ALUFlags(ALUFlags), .WriteP1(WriteP1));
+
+    localparam delay=10us;
+    
+    initial begin
+        CLK=0;
+        Reset=1;
+        WE=1;
+        #delay;
+
+        Reset=0;
+        WE=0;
+        #delay;
+
+        
+    end
+    
+    always #(delay/2)CLK=~CLK;
+endmodule
