4381825ec0f95b53900edd493259160ca64265ca
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-04-27 10:02:13 -0500

    Missing components
    Alu integration

--- Lab4/ALU.sv
+++ Lab4/ALU.sv
@@ -1,24 +1,20 @@
 module ALU #(
-    parameter N = 8 //Number of bits of the arrays
+    parameter N = 32 //Number of bits of the arrays
 ) (
     //Port definition
     input logic [N-1:0] A, //Array to operate with
     input logic [N-1:0] B, //Array to operate with
     input logic unsigned [2:0] Cntr, //Defines the type of operation
-    output logic unsigned [3:0] ALUFlags, //4-bits state of differents operations result: N (Negative), Z (Zero), C (Carry out), V (Overflow)
-    output logic [N-1:0] Result //Result
+    output logic [N-1:0] Result, //Result
+    output logic unsigned [3:0] ALUFlags //4-bits state of differents operations result: N (Negative), Z (Zero), C (Carry out), V (Overflow)
 );
 
     //Parameters
-    localparam Add = 3'b000; //Result = A + B
-    localparam Sub = 3'b001; //Result = A - B
-    localparam And = 3'b010; //Result = A AND B
-    localparam Or = 3'b011; //Result = A OR B
-    localparam Not = 3'b100; //Result = NOT B
-    localparam Move = 3'b101; //Result = B
-    localparam Nimp1 = 3'b110; //No implementation
-    localparam Nimp2 = 3'b111; //No implementation
-
+    localparam Add = 2'b00; //Result = A + B
+    localparam Sub = 2'b01; //Result = A - B
+    localparam And = 2'b10; //Result = A AND B
+    localparam Or = 2'b11; //Result = A OR B
+    
     /*ALUFlags[0] = 1 - Zero Flag
       ALUFlags[0] = 0 - !Zero Flag
       ALUFlags[1] = 1 - Carry Flag
@@ -48,7 +44,7 @@ module ALU #(
 		  sum = A + bTemp + Cntr[0];
         casez(Cntr)
         
-            3'b00? : begin //Addition and substraction
+            2'b0? : begin //Addition and substraction
                 Result = sum[N-1:0];
             end
 
@@ -58,24 +54,6 @@ module ALU #(
 
             Or: begin
                 Result = A | B;
-            end
-
-            Not: begin
-                Result = ~B;
-            end
-
-            Move: begin
-                Result = B;
-            end
-
-            Nimp1: begin
-                $display("Not implemented function");
-                Result = 0;
-            end
-
-            Nimp2: begin
-                $display("Not implemented function");
-                Result = 0;
             end
 
             default: begin
--- Lab4/ALU_tb.sv
+++ Lab4/ALU_tb.sv
@@ -1,36 +1,33 @@
 module ALU_tb ();
 
     //Arrays size
-    localparam N = 8;
+    localparam N = 32;
     localparam delay = 30ps;
     
     //Signals
     logic [N-1:0] A; //Array to operate with
     logic [N-1:0] B; //Array to operate with
     logic unsigned [2:0] Cntr; //Defines the type of operation
-    logic unsigned [3:0] ALUFlags; //4-bits state of differents operations result: N (Negative), Z (Zero), C (Carry out), V (Overflow)
     logic [N-1:0] Result; //Result
+    logic unsigned [3:0] ALUFlags; //4-bits state of differents operations result: N (Negative), Z (Zero), C (Carry out), V (Overflow)
 
     //Definition of ALU
-    ALU alu (.A(A), .B(B), .Cntr(Cntr), .ALUFlags(ALUFlags), .Result(Result));
+    ALU alu (.A(A), .B(B), .Cntr(Cntr), .Result(Result), .ALUFlags(ALUFlags));
 
     //Block
     initial begin
         
-        A = 15;     B = 10;     Cntr = 000; #delay;
-        A = 15;     B = -10;    Cntr = 001; #delay;
-        A = 255;    B = 255;    Cntr = 001; #delay;
-        A = -120;   B = 88;     Cntr = 001; #delay;
-        A = 42;     B = 10;     Cntr = 010; #delay;
-        A = 55;     B = -2;     Cntr = 011; #delay;
-        A = 20;     B = 128;    Cntr = 100; #delay;
-        A = 37;     B = 154;    Cntr = 101; #delay;
-        A = -76;    B = 95;     Cntr = 110; #delay;
-        A = 234;    B = -43;    Cntr = 111; #delay;
+        A = 15;     B = 10;     Cntr = 00; #delay;
+        A = 15;     B = -10;    Cntr = 01; #delay;
+        A = 255;    B = 255;    Cntr = 01; #delay;
+        A = -120;   B = 88;     Cntr = 01; #delay;
+        A = 42;     B = 10;     Cntr = 10; #delay;
+        A = 55;     B = -2;     Cntr = 11; #delay;
+        A = 20;     B = 128;    Cntr = 10; #delay;
+        A = 37;     B = 154;    Cntr = 01; #delay;
+        A = -76;    B = 95;     Cntr = 10; #delay;
+        A = 234;    B = -43;    Cntr = 11; #delay;
 
         $stop;
-
     end
-
-
 endmodule
