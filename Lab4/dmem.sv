91bf03bb721ab36255b23b2f30839f6dd9e03679
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-05-04 10:17:26 -0500

    Part1
    Complete? Missing intr file

--- Lab4/dmem.sv
+++ Lab4/dmem.sv
@@ -1,17 +1,23 @@
-module dmem(input logic clk, we,
+module dmem(input logic clk, we, button,
+            input logic [7:0] sw,
             input logic [31:0] a, wd,
-            output logic [31:0] rd);
+            output logic [31:0] rd,
+            output logic [7:0] deco_displays_seven_segments);
 
+    localparam address = 32'hC000_0000;
     logic [31:0] RAM[63:0];
 
-    initial RAM [0] = 32'hC000_0000;
+    initial RAM [0] = address;
 
-    assign rd = RAM[a[31:2]]; // word aligned
+    //assign rd = RAM[a[31:2]]; // word aligned
+    assign rd = (a == address) ? 32'b0 : (a == address+4) ? 31'b0+button : (a == address+8) ? 24'b0+sw : RAM[a[31:2]];
 
     always_ff @(posedge clk)
         if (we) begin
             RAM[a[31:2]] <= wd;
             
+            if (a == address)
+                deco_displays_seven_segments <= wd[7:0];
         end
         
 endmodule
