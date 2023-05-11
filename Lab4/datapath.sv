481491a6505d7f6ee0fd01663ab4d4d5082e68bb
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-05-11 11:45:15 -0500

    Revert "Datapath"
    This reverts commit 9a6b0bdfe49a8f34e89fafe001a25b66c626ca4b.

--- Lab4/datapath.sv
+++ Lab4/datapath.sv
@@ -1,46 +1,39 @@
 module datapath(input logic clk, reset,
-                input logic PCWrite,
-                input logic AdrSrc,
-                input logic IRWrite,
                 input logic [1:0] RegSrc,
                 input logic RegWrite,
                 input logic [1:0] ImmSrc,
-                input logic ALUSrcA,
-                input logic [1:0] ALUSrcB,
+                input logic ALUSrc,
                 input logic [1:0] ALUControl,
+                input logic MemtoReg,
+                input logic PCSrc,
                 output logic [3:0] ALUFlags,
-                input logic ResultSrc,
-                
-                output logic [31:0] Adr,
-                output logic [31:0] WriteData,
+                output logic [31:0] PC,
+                input logic [31:0] Instr,
+                output logic [31:0] ALUResult, WriteData,
                 input logic [31:0] ReadData);
 
-    logic [31:0] PCNext,PC,Instr,Data;
-    logic [31:0] RD1, RD1Aux, RD2, ExtImm, SrcA, SrcB, ALUOut, Result;
+    logic [31:0] PCNext, PCPlus4, PCPlus8;
+    logic [31:0] ExtImm, SrcA, SrcB, Result;
     logic [3:0] RA1, RA2;
     
-    //PC LOGIC
-    flopenr #(32) pcreg(clk,reset,PCWrite,PCNext,PC);
-    mux2 #(32) pcmux(PC,Result, AdrSrc, Adr);
+    // next PC logic
+    mux2 #(32) pcmux(PCPlus4, Result, PCSrc, PCNext);
+    flopr #(32) pcreg(clk, reset, PCNext, PC);
+    adder #(32) pcadd1(PC, 32'b100, PCPlus4);
+    adder #(32) pcadd2(PCPlus4, 32'b100, PCPlus8);
     
-    //Register file logic
-    flopenr #(32) rfRegister(clk,reset,IRWrite,ReadData,Instr);
-    flopr #(32) rfRegister2(clk, reset,ReadData,Data);
+    // register file logic
     mux2 #(4) ra1mux(Instr[19:16], 4'b1111, RegSrc[0], RA1);
     mux2 #(4) ra2mux(Instr[3:0], Instr[15:12], RegSrc[1], RA2);
     
     regfile rf(clk, RegWrite, RA1, RA2,
-                Instr[15:12], Result, Result,
+                Instr[15:12], Result, PCPlus8,
                 SrcA, WriteData);
+    mux2 #(32) resmux(ALUResult, ReadData, MemtoReg, Result);
+    
     extend ext(Instr[23:0], ImmSrc, ExtImm);
     
-    //ALU logic
-    Register2in #(32) regst(clk,reset,RD1,RD2,RD1Aux,WriteData);
-    mux2 #(32) srcAmux(RD1Aux, PC, ALUSrcA, SrcA);
-    mux3 #(32) srcBmux(WriteData,ExtImm,32'b100,ALUSrcB,SrcB);
+    // ALU logic
+    mux2 #(32) srcbmux(WriteData, ExtImm, ALUSrc, SrcB);
     ALU #(32) alu (.A(SrcA), .B(SrcB), .Cntr(ALUControl), .Result(ALUResult), .ALUFlags(ALUFlags));
-
-    flopr #(32) rfRegister2(clk, reset,ALUResult,ALUOut);
-    mux3 #(32) Alumux(ALUOut,Data,ALUResult,ResultSrc,Result);
-
 endmodule
