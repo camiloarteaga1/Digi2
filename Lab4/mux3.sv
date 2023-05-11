9a6b0bdfe49a8f34e89fafe001a25b66c626ca4b
Camilo Alvarez Muñoz <62733859+camiloam20@users.noreply.github.com>
2023-05-10 23:51:23 -0500

    Datapath

---
+++ Lab4/mux3.sv
@@ -0,0 +1,8 @@
+module mux3 #(parameter WIDTH = 8)
+       (input logic [WIDTH-1:0] d0, d1, d2,
+        input logic [1:0] sel,
+        output logic [WIDTH-1:0] y);
+
+    assign y = (sel == 2'b00) ? d0 : (sel == 2'b01) ? d1 : (sel == 2'b10) ? d2 : 'bx;
+
+endmodule
