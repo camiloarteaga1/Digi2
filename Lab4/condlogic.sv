d88c3463c9999ad1d277fbcb27f3e2fd58c48f6d
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2023-05-11 09:26:14 -0500

    Condlogic Module done

--- Lab4/condlogic.sv
+++ Lab4/condlogic.sv
@@ -2,8 +2,8 @@ module condlogic(input logic clk, reset,
                 input logic [3:0] Cond,
                 input logic [3:0] ALUFlags,
                 input logic [1:0] FlagW,
-                input logic PCS, RegW, MemW,
-                output logic PCSrc, RegWrite, MemWrite);
+                input logic PCS, RegW, MemW, NextPC,
+                output logic PCWrite, RegWrite, MemWrite);
 
     logic [1:0] FlagWrite;
     logic [3:0] Flags; 
@@ -20,7 +20,7 @@ module condlogic(input logic clk, reset,
     assign FlagWrite = FlagW & {2{CondEx}};
     assign RegWrite = RegW & CondEx;
     assign MemWrite = MemW & CondEx;
-    assign PCSrc = PCS & CondEx;
+    assign PCWrite = NextPC | (PCS & CondEx);
 
 endmodule
 
